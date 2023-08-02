9526a97625198514fe61514fa7acd14d413ec4f2
U-NOWAARULAND\zacky <zackyboy135@gmail.com>
2023-08-02 11:37:15 -0700

    cleanup

--- src/server/services/movement.service.ts
+++ src/server/services/movement.service.ts
@@ -2,7 +2,7 @@ import { Service, OnStart, OnInit, Dependency } from "@flamework/core";
 import { ServerFunctions } from "shared/network";
 import { QuarrelGame } from "./quarrelgame.service";
 import { Components } from "@flamework/components";
-import { Participant } from "server/components/participant.component";
+import { EntityState } from "shared/utility/lib";
 
 @Service({})
 export class MovementService implements OnStart, OnInit
@@ -16,15 +16,33 @@ export class MovementService implements OnStart, OnInit
 
     onStart()
     {
-        ServerFunctions.RequestSprint.setCallback((player, sprintState) =>
+        const fetchEntityFromPlayer = (player: Player) =>
         {
             assert(this.quarrelGame.IsParticipant(player), `player ${player} is not a participant`);
             assert(player.Character, "player is not defined");
 
             const participantPlayer = this.quarrelGame.GetParticipantFromCharacter(player.Character)!;
-            participantPlayer.entity?.Sprint(sprintState);
+            assert(participantPlayer.entity, "state entity not defined");
+
+            return participantPlayer.entity;
+        };
+
+        ServerFunctions.RequestSprint.setCallback((player, sprintState) =>
+        {
+            fetchEntityFromPlayer(player).Sprint(sprintState);
 
             return true;
+        });
+
+        ServerFunctions.Crouch.setCallback((player, crouchState) =>
+        {
+
+            return fetchEntityFromPlayer(player).SetState(crouchState);
+        });
+
+        ServerFunctions.Jump.setCallback((player) =>
+        {
+            return fetchEntityFromPlayer(player).SetState(EntityState.Midair);
         });
     }
 }
