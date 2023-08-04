c4d74f3da218abc06ae4a5f53abefb335953dcef
Nowaaru (GitHub) <zackyboy135@gmail.com>
2023-08-04 00:59:58 -0700

    add character models module

---
+++ src/data/models/character/index.ts
@@ -0,0 +1,9 @@
+import { CharacterRigR6 } from "@rbxts/promise-character";
+
+export interface CharacterModel {
+    jane: CharacterRigR6,
+    roblox: CharacterRigR6,
+    shedletsky: CharacterRigR6
+}
+
+export default script as defined as CharacterModel;
