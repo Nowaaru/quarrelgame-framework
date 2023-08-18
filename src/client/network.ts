19f165f2328a589dfd54988f7a80bb7c0ee2f395
Nowaaru <zackyboy135@gmail.com>
2023-08-18 15:48:10 -0700

    add tick listener (that probably won't be used?)

--- src/client/network.ts
+++ src/client/network.ts
@@ -1,4 +1,16 @@
-import { GlobalEvents, GlobalFunctions } from "shared/network";
+import { ClientEvents, GlobalEvents, GlobalFunctions } from "shared/network";
+import type { Frames } from "./controllers/motioninput.controller";
+export interface OnFrame {
+    onFrame(frameTime: Frames, tickRate: number): void;
+}
+
+const onFrameListeners: Set<OnFrame> = new Set();
+ClientEvents.Tick.connect(async (frameTime: Frames, tickRate: number) =>
+{
+    for (const listener of onFrameListeners)
+
+        listener.onFrame(frameTime, tickRate);
+});
 
 export const Events = GlobalEvents.client;
 export const Functions = GlobalFunctions.client;
