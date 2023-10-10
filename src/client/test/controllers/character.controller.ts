8665aeb9a759d5d6e8e97d80a982002d0a55114d
Nowaaru <zackyboy135@gmail.com>
2023-10-10 16:59:38 -0700

    remove test files

--- src/client/test/controllers/character.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from "@flamework/core";
-import { CharacterController2D } from "client/module/character/controller2d";
-
-@Controller({})
-export class CharacterController extends CharacterController2D
-{
-}
