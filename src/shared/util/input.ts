c6ed058a9fcc859083b7459c9fc602297e7008d5
Nowaaru <zackyboy135@gmail.com>
2023-10-12 14:21:19 -0700

    implement other input types (temporary)

--- src/shared/util/input.ts
+++ src/shared/util/input.ts
@@ -43,11 +43,16 @@ export enum InputProcessed
 /**
  * A directional input.
  */
-export type CommandNormal = readonly [Motion, Input];
+export type CommandNormal = [Motion, Input];
 
+/**
+ * An input that translates to an attack in the game.
+ */
 export enum Input
 {
     Dash = "DS",
+    Dust = "DT",
+    Sweep = "SP",
 
     Punch = "P",
     Kick = "K",
@@ -58,6 +63,9 @@ export enum Input
     Burst = "BR",
 }
 
+/**
+ * A direction of movement.
+ */
 export enum Motion
 {
     DownBack = 1,
@@ -78,32 +86,36 @@ export function ConvertMoveDirectionToMotion(moveDirection: Vector3): readonly [
     moveDirection = new Vector3(moveDirection.X, moveDirection.Y, -moveDirection.Z);
 
     const moveDirectionMap: (readonly [Vector3, Motion])[] = ([
-        [new Vector3(0, 1, 0), Motion.Up],
-        [new Vector3(0, -1, 0), Motion.Down],
-        [new Vector3(0, 0, 0.5), Motion.Forward],
-        [new Vector3(0, 0.5, 0.5), Motion.UpForward],
-        [new Vector3(0, 0, 0), Motion.Neutral],
-        [new Vector3(0, 0.5, -0.5), Motion.UpBack],
-        [new Vector3(0, -0.5, -0.5), Motion.DownBack],
-        [new Vector3(0, -0.5, 0.5), Motion.DownForward],
-        [new Vector3(0, 0, -1), Motion.Back],
-    ] as const).map((n) => [n[0].Unit, n[1]]);
+        [ new Vector3(0, 1, 0), Motion.Up ],
+        [ new Vector3(0, -1, 0), Motion.Down ],
+        [ new Vector3(0, 0, 0.5), Motion.Forward ],
+        [ new Vector3(0, 0.5, 0.5), Motion.UpForward ],
+        [ new Vector3(0, 0, 0), Motion.Neutral ],
+        [ new Vector3(0, 0.5, -0.5), Motion.UpBack ],
+        [ new Vector3(0, -0.5, -0.5), Motion.DownBack ],
+        [ new Vector3(0, -0.5, 0.5), Motion.DownForward ],
+        [ new Vector3(0, 0, -1), Motion.Back ],
+    ] as const).map((n) => [ n[0].Unit, n[1] ]);
 
-    for (const [vector, motion] of moveDirectionMap)
+    for (const [ vector, motion ] of moveDirectionMap)
     {
         if (vector.Dot(moveDirection.Unit) > motionDirectionLeniency)
         {
-            const out = [Motion[motion] as keyof typeof Motion, vector.Dot(moveDirection.Unit)] as const;
+            const out = [ Motion[motion] as keyof typeof Motion, vector.Dot(moveDirection.Unit) ] as const;
             if (vector === Vector3.zero)
-            {
-                return [Motion[motion] as keyof typeof Motion, 0];
-            }
+                return [ Motion[motion] as keyof typeof Motion, 0 ];
 
             return out;
         }
     }
 
-    return [Motion[Motion.Neutral] as keyof typeof Motion, Vector3.zero.Dot(moveDirection)];
+    return [ Motion[Motion.Neutral] as keyof typeof Motion, Vector3.zero.Dot(moveDirection) ];
+}
+
+export const isCommandNormal = (attack: unknown[]): attack is [Motion, Input] => !!(Motion[attack[0] as Motion] && Input[attack[1] as never]) && attack.size() === 2;
+export function isInput(input: unknown): input is Input
+{
+    return !!Input[input as never];
 }
 
 export type MotionInput = Array<Motion | Input>;
