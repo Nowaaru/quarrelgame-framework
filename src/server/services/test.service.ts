0539d3c7b3f12f819fdf8832e94d059f5f61f5cc
Nowaaru <nowaaru@proton.me>
2024-06-21 18:37:23 -0700

    the inevitable chickynoid

--- src/server/services/test.service.ts
+++ src/server/services/test.service.ts
@@ -72,6 +72,12 @@ export class TestService implements OnStart, OnInit
             match.Ready.Once(() => {
                 if (match)
                     match.StartMatch();
+
+                task.wait(5);
+                const deps = Dependency<Components>();
+                const entity = deps.getComponents<Entity.Entity>(participant.instance).size()
+                const combatant = deps.getComponents<Combatant>(participant.instance).size()
+
             });
         });
 
